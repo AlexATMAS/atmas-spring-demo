e8cda0d66babd35e2bbbb813a2f59117edc11c6c
ALex ATMAS <atmasbooking@gmail.com>
2022-06-06 14:00:42 +0300

    start

---
+++ src/main/java/pro/sky/atmasspringdemo/AtmasSpringDemoApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.atmasspringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtmasSpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtmasSpringDemoApplication.class, args);
+	}
+
+}
